0a047fa648ab20788877898f649caa54bb507327
supersoulfighter <83781044+supersoulfighter@users.noreply.github.com>
2025-03-17 14:39:22 -0700

    Score 100 sound

--- score.py
+++ score.py
@@ -1,6 +1,6 @@
 from math import floor
 import pygame
-
+from assets import assets
 class Score(pygame.sprite.Sprite):
     def __init__(self, x, y, color, clock, pointsPerMillisecond, font=None):
         super().__init__()
@@ -19,3 +19,5 @@ class Score(pygame.sprite.Sprite):
         self.score += self.clock.get_time() * self.pointsPerMillisecond
         self.image = self.font.render(f"{floor(self.score):05d}", False, self.color)
         self.rect = self.image.get_rect(topleft=(self.x, self.y))
+        if floor(self.score) % 100 == 0 and self.score > 0:
+            assets["sounds/score"].play()
